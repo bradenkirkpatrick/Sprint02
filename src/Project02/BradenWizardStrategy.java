a77eae29c5935906804eaaed2755d75726341b98
braden <kirkpatrickb02@moravian.edu>
2023-03-09 10:59:44 -0500

    asdsa

--- src/Project02/BradenWizardStrategy.java
+++ src/Project02/BradenWizardStrategy.java
@@ -1,5 +1,13 @@
 package Project02;
 
-public class BradenWizardStrategy {
-    
+public class BradenWizardStrategy extends People{
+    public BradenWizardStrategy(String nation, String tribe, int lifePoints, Strategy warriorStrategy) {
+        super(nation, tribe, PeopleType.wizard, lifePoints, warriorStrategy);
+        myDescription = "\tBraden Warrior";
+    }
+    @Override
+    public int encounterLifePoints(People me, People otherPerson) {
+        return encounterStrategy.strategy(me, otherPerson);
+    }
+
 }
