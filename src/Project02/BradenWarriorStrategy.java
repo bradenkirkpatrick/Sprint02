73e19b539adf70a94ecc608e7bd0a6ef91392958
braden <kirkpatrickb02@moravian.edu>
2023-03-16 10:24:04 -0400

    Braden: wizard and warrior strats

--- src/Project02/BradenWarriorStrategy.java
+++ src/Project02/BradenWarriorStrategy.java
@@ -1,5 +1,30 @@
 package Project02;
 
-public class BradenWarriorStrategy {
-    
+public class BradenWarriorStrategy implements Strategy{
+    /**
+     * Warrior strategy is generally aggressive and returns maximum amount of life
+     * points if players are from different nations. If from the same nation then
+     * player will transfer life points.
+     * @param me - person to get life points for.
+     * @param otherPerson - the opponent of the encounter
+     * @return - Life points to use in the encounter
+     */
+    public int strategy(People me, People otherPerson) {
+        int lifePoints = 0;
+        if(me.getNation() == otherPerson.getNation()) {
+            if(PeopleType.wizard == otherPerson.getType()){
+                lifePoints = Math.min(me.getLifePoints() - 1, 100 - otherPerson.getLifePoints());
+            } else {
+                if(otherPerson.getLifePoints() < me.getLifePoints()) {
+                    lifePoints = 0;
+                } else {
+                    lifePoints = Math.min(me.getLifePoints() - 1, 100 - otherPerson.getLifePoints());
+                }
+            }
+        } else {
+            lifePoints = me.getLifePoints();
+        }
+        return lifePoints;
+    }
+
 }
--- src/Project02/BradenWizardStrategy.java
+++ src/Project02/BradenWizardStrategy.java
@@ -1,13 +1,29 @@
 package Project02;
 
-public class BradenWizardStrategy extends People{
-    public BradenWizardStrategy(String nation, String tribe, int lifePoints, Strategy warriorStrategy) {
-        super(nation, tribe, PeopleType.wizard, lifePoints, warriorStrategy);
-        myDescription = "\tBraden Warrior";
+public class BradenWizardStrategy implements Strategy{
+    /**
+     * Warrior strategy is generally aggressive and returns maximum amount of life
+     * points if players are from different nations. If from the same nation then
+     * player will transfer life points.
+     * @param me - person to get life points for.
+     * @param otherPerson - the opponent of the encounter
+     * @return - Life points to use in the encounter
+     */
+    public int strategy(People me, People otherPerson) {
+        int lifePoints = 0;
+        if(me.getNation() == otherPerson.getNation()) {
+            if(PeopleType.wizard == otherPerson.getType()){
+                if(otherPerson.getLifePoints() < me.getLifePoints()) {
+                    lifePoints = 0;
+                } else {
+                    lifePoints = Math.min(me.getLifePoints() - 1, 100 - otherPerson.getLifePoints());
+                }
+            } else {
+                lifePoints = 0;
+            }
+        } else {
+            lifePoints = me.getLifePoints();
+        }
+        return lifePoints;
     }
-    @Override
-    public int encounterLifePoints(People me, People otherPerson) {
-        return encounterStrategy.strategy(me, otherPerson);
-    }
-
 }
