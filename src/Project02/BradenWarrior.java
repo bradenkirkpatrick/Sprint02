ff9d58415da492bc98ab444ab9a4ebfad4903ec5
braden <kirkpatrickb02@moravian.edu>
2023-03-16 10:38:11 -0400

    Braden Warrior and Wizard

---
+++ src/Project02/BradenWarrior.java
@@ -0,0 +1,13 @@
+package Project02;
+
+public class BradenWarrior extends People {
+    BradenWarrior(String nation, String tribe, int lifePoints, Strategy strategy) {
+        super(nation, tribe, PeopleType.wizard, lifePoints, strategy);
+        myDescription = "Now i am become death, destroyer of worlds";
+
+    }
+    @Override
+    public int encounterLifePoints(People me, People otherPerson) {
+        return encounterStrategy.strategy(me, otherPerson);
+    }
+}
