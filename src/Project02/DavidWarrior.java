46c8dea6de208bf275bac5abf2847f1cca75fa85
badlydrawnface <bdface@proton.me>
2023-03-16 10:20:28 -0400

    [david] fixed names

--- src/Project02/DavidWarrior.java
+++ src/Project02/DavidWarrior.java
@@ -3,7 +3,7 @@ package Project02;
 public class DavidWarrior extends People {
     public DavidWarrior(String nation, String tribe, int lifePoints, Strategy warriorStrategy) {
         super(nation, tribe, PeopleType.warrior, lifePoints, warriorStrategy);
-        myDescription = "\tSchaper Warrior";
+        myDescription = "\tDavid Warrior";
     }
     @Override
     public int encounterLifePoints(People me, People otherPerson) {
