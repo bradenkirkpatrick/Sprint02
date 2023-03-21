72be334ed2ac9850e9d9bdcc6a51d0b946513d48
Robert <cruzr@moravian.edu>
2023-03-21 11:00:00 -0400

    added my warrior method

--- src/Project02/RobertWarriorStrategy.java
+++ src/Project02/RobertWarriorStrategy.java
@@ -2,8 +2,18 @@ package Project02;
 
 public class RobertWarriorStrategy implements Strategy {
 
-    @Override
+
     public int strategy(People me, People otherPerson) {
-        return 0;
+        int lifePoints = 0;
+        if (me.getNation() == otherPerson.getNation()) {
+            if (otherPerson.getTribe() != me.getTribe()) { // if tribe is not mine attack
+                lifePoints = (int) (me.getLifePoints() / 2);
+            } else {
+                lifePoints = (int) (me.getLifePoints() / 4); //still attack because why not
+            }
+        }
+        return lifePoints;
     }
 }
+
+//no healing for warriors because warriors are to tough to heal
