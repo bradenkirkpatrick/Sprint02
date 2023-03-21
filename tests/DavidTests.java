59bda5d3b77597fc57038fb8780610475397e29b
badlydrawnface <bdface@proton.me>
2023-03-21 11:00:50 -0400

    trying encounters

--- tests/DavidTests.java
+++ tests/DavidTests.java
@@ -5,11 +5,20 @@ import static org.junit.jupiter.api.Assertions.*;
 public class DavidTests {
     
     @Test
-    public void pepegaDriving() {
+    public void testPeople() {
         DavidWarrior dw = new DavidWarrior("Papal States", "Catholicism", 69, new DavidWarriorStrategy());
         DavidWizard dwz = new DavidWizard("DEVS VVLT", "iesus nazarenus rex iudeorum", 33, new DavidWizardStrategy());
         assertEquals(69, dw.getLifePoints());
         assertEquals(33, dwz.getLifePoints());
+    }
+
+    @Test
+    public void testEncounter() {
+        World w = new World();
+        DavidWizard wiz = new DavidWizard("the moon", "a tribe", 87, new DavidWizardStrategy());
+        DavidWizard wiz2 = new DavidWizard("the moon", "a tribe", 87 , new DavidWizardStrategy());
+        w.encounter(wiz.getLifePoints(), wiz2.getLifePoints());
+        
     }
 }
 
