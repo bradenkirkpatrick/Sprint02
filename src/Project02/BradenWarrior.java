91a9f07c62bce2625b69f9ee6d0912b764f1b2e5
braden <kirkpatrickb02@moravian.edu>
2023-03-28 10:34:19 -0400

    minor changes and tests

--- src/Project02/BradenWarrior.java
+++ src/Project02/BradenWarrior.java
@@ -3,8 +3,7 @@ package Project02;
 public class BradenWarrior extends People {
     public BradenWarrior(String nation, String tribe, int lifePoints, Strategy strategy) {
         super(nation, tribe, PeopleType.wizard, lifePoints, strategy);
-        myDescription = "Now i am become death, destroyer of worlds";
-
+        myDescription = ". . .";
     }
     @Override
     public int encounterLifePoints(People me, People otherPerson) {
--- tests/BradenTests.java
+++ tests/BradenTests.java
@@ -1,24 +1,41 @@
 import Project02.*;
+
 import org.junit.jupiter.api.Test;
 public class BradenTests {
-    public static void main(String[] args) {
-        testNewPeople();
-    }
     //String nation, String tribe, int lifePoints, Strategy strategy
     @Test
-    public static void testNewPeople(){
-        World america = new World();
-
-        Tribe bulgarians = new Tribe("Bulgaria", "Soyia", 1000);
-        People Wiz = new BradenWizard("Eins", "A", 200, new BradenWizardStrategy());
-        People War = new BradenWarrior("Zwei", "B", 200, new BradenWarriorStrategy());
-        System.out.println(bulgarians);
-        System.out.println(Wiz);
-        System.out.println(War);
+    public static void testSameNationWarxWiz(){
+        World world = new World();
+        People wiz = new BradenWizard("Eins", "A", 20, new BradenWizardStrategy());
+        People war = new BradenWarrior("Eins", "B", 20, new BradenWarriorStrategy());
+        assert(wiz.encounterLifePoints(wiz, war) == 0);
+        assert(war.encounterLifePoints(war, wiz) == 19);
+        world.encounter(war.encounterLifePoints(war, wiz), wiz.encounterLifePoints(wiz, war));
+        assert(war.isPersonAlive() == false);
+        
     }
 
     @Test
-    public static void testLifePoints(){
+    public static void testSameNationWizxWiz(){
+        World world = new World();
+        People wiz1 = new BradenWizard("Eins", "A", 20, new BradenWizardStrategy());
+        People wiz2 = new BradenWarrior("Eins", "B", 20, new BradenWizardStrategy());
+        assert(wiz1.encounterLifePoints(wiz1, wiz2) == 0);
+        assert(wiz2.encounterLifePoints(wiz2, wiz1) == 0);
+        world.encounter(wiz1.encounterLifePoints(wiz1, wiz2), wiz2.encounterLifePoints(wiz2, wiz1));
+        assert(wiz1.getLifePoints() == 20);
+        assert(wiz2.getLifePoints() == 20);
+    }
 
+    @Test
+    public static void testDifferentNation(){
+        World world = new World();
+        People wiz1 = new BradenWizard("Eins", "A", 20, new BradenWizardStrategy());
+        People wiz2 = new BradenWarrior("Zwei", "B", 20, new BradenWizardStrategy());
+        assert(wiz1.encounterLifePoints(wiz1, wiz2) == 20);
+        assert(wiz2.encounterLifePoints(wiz2, wiz1) == 20);
+        world.encounter(wiz1.encounterLifePoints(wiz1, wiz2), wiz2.encounterLifePoints(wiz2, wiz1));
+        assert(wiz1.getLifePoints() < 20);
+        assert(wiz2.getLifePoints() < 20);
     }
 }
