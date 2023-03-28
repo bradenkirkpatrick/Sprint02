9fe99c87fd1d3d24a5a219038bcdc5fe55cd5df8
Robert <cruzr@moravian.edu>
2023-03-28 10:58:30 -0400

    added javadoc to PeopleType

--- src/Project02/PeopleType.java
+++ src/Project02/PeopleType.java
@@ -7,12 +7,21 @@ public enum PeopleType
 
     private String description;
 
+    /**
+     *
+     * @param types
+     * PeopleType constructor takes in a String called tyoes and sets it to the current description
+     */
     PeopleType (String types)
     {
         description = types;
     }
 
 
+    /**
+     * getDescription method returns the Description of the PersonType
+     * @return description
+     */
     public String getDescription()
     {
         return description;
