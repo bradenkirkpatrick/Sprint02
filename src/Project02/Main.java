7c3de72dcc6c4aeb9d26d87d97e1ecdc9b89ed2b
Robert <cruzr@moravian.edu>
2023-03-28 10:22:46 -0400

    added javadoc to main

--- src/Project02/Main.java
+++ src/Project02/Main.java
@@ -7,7 +7,11 @@ import Project02.PlayGame;
 public class Main
 {
 
-
+    /**
+     *
+     * @param args
+     * the main method instantiates a new play game object
+     */
     public static void main(String[] args)
     {
         PlayGame game = new PlayGame();
