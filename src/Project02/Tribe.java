43c467be05a55373d1483570100e8264194655fd
Robert <cruzr@moravian.edu>
2023-04-04 11:09:44 -0400

    fixed

--- src/Project02/Tribe.java
+++ src/Project02/Tribe.java
@@ -22,23 +22,22 @@ public class Tribe
         nationName = nation;
         tribeName = tribe;
         tribeLifePoints = lifePoints / 5;
-        if(nationName == "minions")
-            for(int i = 0; i < 5; i++){
-                members.add(new SchaperWarrior(nationName, tribeName, tribeLifePoints, new WizardStrategy()));
-                members.add(new SchaperWizard(nationName, tribeName, tribeLifePoints, new WarriorStrategy()));
-        } else if (nationName == "Sussy Imposters") 
-            for(int i = 0; i < 5; i++){
-                members.add(new DavidWarrior(nationName, tribeName, tribeLifePoints, new DavidWarriorStrategy()));
-                members.add(new DavidWizard(nationName, tribeName, tribeLifePoints, new DavidWizardStrategy()));
-        } else if (nationName == "Robs Nation") 
-            for(int i = 0; i < 5; i++){
-                members.add(new RobertWarrior(nationName, tribeName, tribeLifePoints, new RobertWarriorStrategy()));
-                members.add(new RobertWizard(nationName, tribeName, tribeLifePoints, new RobertWizardStrategy()));
-        } else if (nationName == "Bradens Nation") 
-            for(int i = 0; i < 5; i++){
-                members.add(new BradenWarrior(nationName, tribeName, tribeLifePoints, new BradenWarriorStrategy()));
-                members.add(new BradenWizard(nationName, tribeName, tribeLifePoints, new BradenWizardStrategy()));
-            }
+        if(nationName == "Minions"){
+            members.add(new SchaperWarrior(nationName, tribeName, tribeLifePoints, new WizardStrategy()));
+            members.add(new SchaperWizard(nationName, tribeName, tribeLifePoints, new WarriorStrategy()));
+        }
+        else if (nationName == "Sussy Impostors") {
+            members.add(new DavidWarrior(nationName, tribeName, tribeLifePoints, new DavidWarriorStrategy()));
+            members.add(new DavidWizard(nationName, tribeName, tribeLifePoints, new DavidWizardStrategy()));
+        }
+        else if(nationName == "Robs Nation"){
+            members.add(new RobertWarrior(nationName, tribeName, tribeLifePoints, new RobertWarriorStrategy()));
+            members.add(new RobertWizard(nationName, tribeName, tribeLifePoints, new RobertWizardStrategy()));
+        }
+        else if (nationName == "Braden Nation"){
+            members.add(new BradenWarrior(nationName, tribeName, tribeLifePoints, new BradenWarriorStrategy()));
+            members.add(new BradenWizard(nationName, tribeName, tribeLifePoints, new BradenWizardStrategy()));
+        }
         for(int i = 0; i < members.size(); i++)
             livingMembers.addAll(members);
     }
