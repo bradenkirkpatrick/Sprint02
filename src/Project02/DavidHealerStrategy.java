1a26345d93fa7abcda2b955a29755418881bc024
badlydrawnface <bdface@proton.me>
2023-04-13 10:42:40 -0400

    implemented Healer, streamlined UML
    fixed a mistake

---
+++ src/Project02/DavidHealerStrategy.java
@@ -0,0 +1,9 @@
+package Project02;
+
+public class DavidHealerStrategy implements Strategy {
+    @Override
+    // TODO
+    public int strategy(People me, People otherPerson) {
+        return 0;
+    }
+}
