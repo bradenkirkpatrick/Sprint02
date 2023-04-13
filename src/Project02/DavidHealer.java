921356fbfea03ab5287198a295d6040c412476ea
badlydrawnface <bdface@proton.me>
2023-04-13 10:42:40 -0400

    implemented Healer, streamlined UML

---
+++ src/Project02/DavidHealer.java
@@ -0,0 +1,12 @@
+package Project02;
+
+public class DavidHealer extends People {
+    public DavidHealer(String nation, String tribe, int lifePoints, Strategy strategy) {
+        super(nation, tribe, PeopleType.healer, lifePoints, strategy)
+        myDescription = "Jahwol!";
+    }
+
+    public int encounterLifePoints(People me, People otherPerson) {
+        return encounterStrategy.strategy(me, otherPerson);
+    }
+}
