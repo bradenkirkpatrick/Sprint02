494eb6bb12e65e683b1b2548cc0a604d857e1744
Robert <cruzr@moravian.edu>
2023-04-18 10:05:39 -0400

    refactored healer

--- src/Project02/RobertHealerStrategy.java
+++ src/Project02/RobertHealerStrategy.java
@@ -1,26 +1,16 @@
 package Project02;
-public class RobertHealerStrategy extends Die implements Strategy {
 
+public class RobertHealerStrategy extends Die implements Strategy {
 
     public int strategy(People me, People otherPerson) {
         int lifePoints = 0;
         int intendedHealingPoints = 100;
-        if (me.getNation() != otherPerson.getNation()) {
+        if (me.getNation() == otherPerson.getNation()) {
+            Die die = new Die(intendedHealingPoints);
+            int actualHealPoints = die.getRand();
+            lifePoints = (int) (otherPerson.getLifePoints() + actualHealPoints);
+        } else {
             lifePoints = -me.getLifePoints();// - run away
-        }
-        else
-        {
-            if (otherPerson.getType() == PeopleType.warrior) {
-                Die die = new Die(intendedHealingPoints);
-                int actualHealPoints = die.getRand();
-                lifePoints = (int) (otherPerson.getLifePoints() + actualHealPoints);
-            }
-            else if (otherPerson.getType() == PeopleType.wizard) {
-
-            }
-            else if(otherPerson.getType() == PeopleType.healer) {
-
-            }
         }
         return lifePoints;
     }
