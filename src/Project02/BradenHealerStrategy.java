ee2bd60085011ed024da40d40c9dada99adbe7aa
Robert <cruzr@moravian.edu>
2023-04-27 10:49:34 -0400

    braden healer strat done

--- src/Project02/BradenHealerStrategy.java
+++ src/Project02/BradenHealerStrategy.java
@@ -1,2 +1,19 @@
-package Project02;public class BradenHealerStrategy {
+package Project02;
+
+public class BradenHealerStrategy implements Strategy {
+    public int strategy(People me, People otherPerson) {
+        int lifePoints = 0;
+        int intendedHealingPoints = 100;
+        if (me.getNation() == otherPerson.getNation()) {
+            Die die = Die.getInstance(intendedHealingPoints);
+            int actualHealPoints = die.roll();
+            if (otherPerson.getType() == PeopleType.wizard) {
+                lifePoints = (int) (otherPerson.getLifePoints() + (actualHealPoints / 2));
+            } else if (otherPerson.getType() == PeopleType.warrior) {
+                lifePoints = (int) (otherPerson.getLifePoints() + actualHealPoints);
+            }
+            lifePoints = -me.getLifePoints();// - run away
+        }
+        return lifePoints;
+    }
 }
