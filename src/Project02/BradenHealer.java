29e25990d36b43bd1b77d16c0f846f0646c7262e
Robert <cruzr@moravian.edu>
2023-04-27 10:42:35 -0400

    added Braden Healer

---
+++ src/Project02/BradenHealer.java
@@ -0,0 +1,14 @@
+package Project02;
+
+public class BradenHealer extends People{
+
+    BradenHealer(String nation, String tribe, int lifePoints, Strategy BradenHealerStrategy) {
+        super(nation, tribe, PeopleType.healer, lifePoints, BradenHealerStrategy);
+        myDescription = "\tBradem Healer";
+    }
+    @Override
+    public int encounterLifePoints(People me, People otherPerson) {
+        return encounterStrategy.strategy(me, otherPerson);
+    }
+
+}
