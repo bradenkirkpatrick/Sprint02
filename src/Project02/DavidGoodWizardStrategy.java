8d77d89bc274c407237338d071c406e2c82cc3cc
badlydrawnface <bdface@proton.me>
2023-04-27 10:20:43 -0400

    finished wizard strategy

--- src/Project02/DavidGoodWizardStrategy.java
+++ src/Project02/DavidGoodWizardStrategy.java
@@ -8,10 +8,7 @@ public class DavidGoodWizardStrategy implements Strategy {
     public int strategy(People me, People otherPerson) {
         int lifePoints = 0;
         if (me.getNation().equals(otherPerson.getNation())) {
-            if (otherPerson.getType().equals(PeopleType.wizard)) {
-                // TODO
-            }
-            else if (otherPerson.getType().equals(PeopleType.healer)) {
+            if (otherPerson.getType().equals(PeopleType.healer)) {
                 // maybe the wizard gives some health to the healer ;)
                 Die die2 = Die.getInstance(2);
                 if (die2.roll() == 0) {
@@ -25,6 +22,7 @@ public class DavidGoodWizardStrategy implements Strategy {
             }
         }
         else {
+            // generic attack
             Die d = Die.getInstance(35);
             int takePoints = d.roll();
             lifePoints = (int) (otherPerson.getLifePoints() -  takePoints);
